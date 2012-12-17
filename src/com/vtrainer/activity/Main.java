aa3de9db7b3cd5a2c175abf05e7fcee99e94f9dd
Denys_Cherepanin <Denys_Cherepanin@EPUAKYIL0218.kyiv.epam.com>
2012-12-17 16:10:23 +0200

    added base implementation of support of ukrainian locale

--- src/com/vtrainer/activity/Main.java
+++ src/com/vtrainer/activity/Main.java
@@ -1,7 +1,10 @@
 package com.vtrainer.activity;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,32 +12,43 @@ import android.widget.Button;
 
 public class Main extends Activity implements OnClickListener {
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-    Button btnVocabulary = (Button) findViewById(R.id.vocabulary);
-    btnVocabulary.setOnClickListener(this);
+        Button btnVocabulary = (Button) findViewById(R.id.vocabulary);
+        btnVocabulary.setOnClickListener(this);
 
-    Button btnTrainings = (Button) findViewById(R.id.trainings);
-    btnTrainings.setOnClickListener(this);
-  }
-
-  @Override
-  public void onClick(View v) {
-  	Intent intent;
-    switch (v.getId()) {
-      case R.id.vocabulary:
-        intent = new Intent(this, VocabularyActivity.class);
-      break;
-      case R.id.trainings:
-        intent = new Intent(this, TrainingsActivity.class);
-      break;
-    default:
-      return;
+        Button btnTrainings = (Button) findViewById(R.id.trainings);
+        btnTrainings.setOnClickListener(this);
     }
 
-    startActivity(intent);
-  }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Locale locale = new Locale("ua");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+        case R.id.vocabulary:
+            intent = new Intent(this, VocabularyActivity.class);
+            break;
+        case R.id.trainings:
+            intent = new Intent(this, TrainingsActivity.class);
+            break;
+        default:
+            return;
+        }
+
+        startActivity(intent);
+    }
 }
